76a9027463dfd7c9fb5ed33ad3ce3cd35d693da3
kiesel <kiesel@d2cacbed-c0f6-0310-851a-9dad52fc623a>
2010-08-07 14:52:58 +0000

    - indicate no further processing should be done after redirect - extend debug to be more readable
    git-svn-id: http://svn.xp-framework.net/xpforge/trunk/projects/www/uska@12502 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- src/de/uska/scriptlet/UskaScriptlet.class.php
+++ src/de/uska/scriptlet/UskaScriptlet.class.php
@@ -75,25 +75,29 @@
       return newinstance('scriptlet.RequestAuthenticator', array(), '{
         public function authenticate($request, $response, $context) {
           $cat= Logger::getInstance()->getCategory();
-          $cat->debug("Authentication to be performed for", $request->getURL(), $context);
+          $cat->debug($this->getClassName(), "~ Authentication to be performed for", $request->getURL()->getURL(), "?");
 
           if (!$request->state->requiresAuthentication()) {
-            $cat->debug("No auth because state not requiring it.");
+            $cat->debug($this->getClassName(), "~ No auth because state not requiring it.");
             return; // Skip
           }
 
           // HACK: Actually, we should use $context->getUser(), but currently context
           // is never passed in. So, apply workaround instead.
           if (1 == $request->session->getValue("logged-in")) {
-            $cat->debug("No auth because user is already authenticated");
+            $cat->debug($this->getClassName(), "~ No auth because user is already authenticated");
             return; // OK
           }
 
-          $cat->debug("Authentication required - initiating login...");
+          $cat->info("Authentication required - initiating login...");
 
           // Dispatch - LoginHandler will redirect to original state
           $request->session->putValue("authreturn", $request->getURL());
+		  $cat->debug($this->getClassName(), "~ Return URL is", $request->getURL()->getURL());
           $response->forwardTo("login");
+		  
+		  // Indicate no further processing is to be done
+		  return FALSE;
         }
       }');
     }
